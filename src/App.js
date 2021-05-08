42b2e6e7e5252c2cd462da812ed4c7b9f9e484af
vaibhavbuccha <jainvaibhav415@gmail.com>
2021-05-08 19:25:54 +0530

    'Meetups-Listing'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layouts/Layout";
 function App() {
-  return <div></div>;
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupsPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
